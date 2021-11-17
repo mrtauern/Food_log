c1ed31f5a607095e52b83cd8149f3432c9802cbe
niklasld <36439309+niklasld@users.noreply.github.com>
2021-11-17 10:12:38 +0100

    updated log_types.sql

--- src/main/resources/data/log_types.sql
+++ src/main/resources/data/log_types.sql
@@ -1,9 +1,7 @@
 INSERT INTO log_type VALUES
     (NULL,'Breakfast'),
     (NULL,'Lunch'),
-    (NULL,'Afternoon'),
     (NULL,'Dinner'),
-    (NULL,'Evening'),
     (NULL,'Miscellaneous'),
     (NULL,'Weight'),
     (NULL,'Exercise');
