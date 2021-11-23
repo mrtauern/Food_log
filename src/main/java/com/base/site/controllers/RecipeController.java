e8508605be806e965acc3c5c17d7007b55118385
niklasld <36439309+niklasld@users.noreply.github.com>
2021-11-23 10:38:48 +0100

    fixed

--- src/main/java/com/base/site/controllers/RecipeController.java
+++ src/main/java/com/base/site/controllers/RecipeController.java
@@ -132,14 +132,12 @@ public class RecipeController {
 
 
         if(loggedInUser.getId() == recipe.getFkUser().getId()) {
-            log.info("hello1");
             recipeFood.setRecipe(recipe);
             recipeFood.setFood(foodService.findById(foodId));
             recipeFoodService.save(recipeFood);
 
             return REDIRECT+ADD_FOOD_TO_RECIPE+"/"+recipeId;
         } else {
-            log.info("hello2");
             return REDIRECT+RECIPES;
         }
 
