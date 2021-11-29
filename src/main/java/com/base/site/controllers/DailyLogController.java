7f407fcc3db7271a40d1c5a38570a4536eefb580
niklasld <36439309+niklasld@users.noreply.github.com>
2021-11-29 13:07:26 +0100

    dailylogcontroller slimmed by 100 lines, moved to dailylogserviceimpl

--- src/main/java/com/base/site/controllers/DailyLogController.java
+++ src/main/java/com/base/site/controllers/DailyLogController.java
@@ -1,9 +1,6 @@
 package com.base.site.controllers;
 
-import com.base.site.models.DailyLog;
-import com.base.site.models.Exercise;
-import com.base.site.models.Food;
-import com.base.site.models.Users;
+import com.base.site.models.*;
 import com.base.site.services.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -68,7 +65,6 @@ public class DailyLogController {
         }
 
         return REDIRECT + DAILY_LOG + "/" + outputFormat.format(selectedDate);
-        //return null;
     }
 
     @GetMapping({"/dailyLog", "/dailyLog/{date}"})
@@ -76,132 +72,39 @@ public class DailyLogController {
     public String dailyLog(Model model,@Param("keyword") String keyword, @PathVariable(required = false, value = "date") String dateString) throws ParseException {
         log.info("Getmapping called for dailylog for specific date: "+dateString);
 
-        DailyLog weightLog = new DailyLog();
-
-        Food nutrition = new Food("nutrition",0.0,0.0,0.0,0.0,0.0);
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        LocalDate date = dateString == null ? LocalDate.now() : LocalDate.parse(dateString, formatter);
-
-        List<DailyLog> dailyLogs = dailyLogService.findAll();
-        List<DailyLog> dailyLogsFoods = new ArrayList<>();
-        List<DailyLog> dailyLogsBreakfast = new ArrayList<>();
-        List<DailyLog> dailyLogsLunch = new ArrayList<>();
-        List<DailyLog> dailyLogsDinner = new ArrayList<>();
-        List<DailyLog> dailyLogsMiscellaneous = new ArrayList<>();
-        List<DailyLog> dailyLogsPrivateFoods = new ArrayList<>();
-        List<DailyLog> dailyLogsExercises = new ArrayList<>();
-
-        for (DailyLog dailyLog: dailyLogs) {
-            if(dailyLog.getDatetime().equals(date) && usersService.getLoggedInUser().getId() == dailyLog.getFkUser().getId()) {
-
-                String sDatetime = dailyLog.getDatetime().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-                dailyLog.setSDatetime(sDatetime);
-
-                if(dailyLog.getFood() != null || dailyLog.getPrivateFood() != null) {
-                    dailyLogsFoods.add(dailyLog);
-                    String logType = dailyLog.getFkLogType().getType();
-
-                    switch (logType){
-                        case "Breakfast":
-                            dailyLogsBreakfast.add(dailyLog);
-                            nutrition = foodService.setAddFoodNutritionFromDailylog(nutrition, dailyLog);
-                            break;
-                        case "Lunch":
-                            dailyLogsLunch.add(dailyLog);
-                            nutrition = foodService.setAddFoodNutritionFromDailylog(nutrition, dailyLog);
-                            break;
-                        case "Dinner":
-                            dailyLogsDinner.add(dailyLog);
-                            nutrition = foodService.setAddFoodNutritionFromDailylog(nutrition, dailyLog);
-                            break;
-                        case "Miscellaneous":
-                            dailyLogsMiscellaneous.add(dailyLog);
-                            nutrition = foodService.setAddFoodNutritionFromDailylog(nutrition, dailyLog);
-                            break;
-                        case "Weight":
-                            weightLog = dailyLog;
-                            break;
-                        default:
-                            log.info("UPS... Something went wrong!");
-                    }
-
-                }else if (dailyLog.getFkExercise() != null){
-                    dailyLogsExercises.add(dailyLog);
-                }else if(dailyLog.getFkLogType().getType().equals("Weight")) {
-                    weightLog = dailyLog;
-                }
-            }
-        }
-
-        Date currentDate = new Date();
-        LocalDate today = currentDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        model.addAttribute("today", today.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
-        model.addAttribute("sSelectedDate", date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-
-        model.addAttribute("foods", dailyLogsFoods);
-        model.addAttribute("breakfasts", dailyLogsBreakfast);
-        model.addAttribute("lunches", dailyLogsLunch);
-        model.addAttribute("dinners", dailyLogsDinner);
-        model.addAttribute("miscellaneous", dailyLogsMiscellaneous);
-        //model.addAttribute("pfoods", dailyLogsPrivateFoods);
-        model.addAttribute("exercises", dailyLogsExercises);
-        model.addAttribute("keyword", keyword);
-
-        // +/- Day
-        model.addAttribute("tomorrow", date.plusDays(1).format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
-        model.addAttribute("yesterday", date.minusDays(1).format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
-
-        // +/- Week
-        model.addAttribute("nextWeek", date.plusWeeks(1).format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
-        model.addAttribute("previousWeek", date.minusWeeks(1).format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
-
-        // +/- Month
-        model.addAttribute("nextMonth", date.plusMonths(1).format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
-        model.addAttribute("previousMonth", date.minusMonths(1).format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
-
-        model.addAttribute("bmr", usersService.getLoggedInUser().getBMR(usersService.getLatestWeight(date).getAmount()));
-        model.addAttribute("kcalUsed", dailyLogService.getKcalUsed(date, usersService.getLoggedInUser()));
-        model.addAttribute("kcalLeft", dailyLogService.getKcalLeft(date, usersService.getLoggedInUser()));
-        model.addAttribute("nutrition", nutrition);
-        log.info("nutrition ------ fat: "+nutrition.getFat()+" carbs: "+nutrition.getCarbohydrates()+" protein: "+nutrition.getProtein());
-
-        model.addAttribute("weight", weightLog);
-        //model.addAttribute("date", dateString);
-
-        model.addAttribute("selectedPage", "dailyLog");
-        model.addAttribute("user_name", usersService.getLoggedInUser().getFirstname() + " " + usersService.getLoggedInUser().getLastname());
-        model.addAttribute("user_gender", usersService.getLoggedInUser().getUserType().getType());
+        model = dailyLogService.getDailyLogModels(usersService.getLoggedInUser(), dateString, model, keyword);
 
         return DAILY_LOG;
     }
 
     @GetMapping("/addCurrentWeight")
     public String addCurrentWeight(Model model, @Param("keyword") String keyword) {
+        log.info("  get mapping addCurrentWeight is called");
+
         List<DailyLog> dailyLog = dailyLogService.findAllByKeyword(keyword);
+
         model.addAttribute("dailyLog", dailyLog);
         model.addAttribute("keyword", keyword);
-        log.info("  get mapping addCurrentWeight is called");
 
         return ADD_WEIGHT;
     }
 
     @GetMapping("/createCurrentWeight")
     public String createCurrentWeight( Model model, DailyLog dailyLog) {
-        model.addAttribute("dailyLog", dailyLog);
         log.info("  Get mapping createCurrentWeight is called ");
+
+        model.addAttribute("dailyLog", dailyLog);
 
         return CREATE_WEIGHT;
     }
     @PostMapping("/saveCurrentWeight")
-    public String saveCurrentWeight(@ModelAttribute("dailyLog") DailyLog dailyLog, Model model) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        Users loggedInUser = usersService.findByUserName(auth.getName());
-        dailyLog.setFkLogType(logTypeService.findByType("Weight"));
-        dailyLog.setFkUser(loggedInUser);
-        log.info("weight"+dailyLog.getAmount());
-        dailyLogService.save(dailyLog);
+    public String saveCurrentWeight(@ModelAttribute("dailyLog") DailyLog dailyLog) {
         log.info("  Post Mapping saveCurrentWeight is called ");
+
+        dailyLog.setFkLogType(logTypeService.findByType("Weight"));
+        dailyLog.setFkUser(usersService.getLoggedInUser());
+
+        dailyLogService.save(dailyLog);
 
         return  REDIRECT + DAILY_LOG;
 
@@ -209,17 +112,18 @@ public class DailyLogController {
 
     @GetMapping("/updateCurrentWeight/{id}")
     public String updateCurrentWeight(@PathVariable(value = "id") Long id, Model model) {
-        DailyLog dailyLog = dailyLogService.findById(id);
-        model.addAttribute("dailyLog", dailyLog);
-        log.info("  GetMapping updateCurrentWeight is called ");
+        log.info("  GetMapping updateCurrentWeight is called with id: "+id);
+
+        model.addAttribute("dailyLog", dailyLogService.findById(id));
 
         return UPDATE_WEIGHT;
     }
 
     @GetMapping("/deleteCurrentWeight/{id}")
     public String deleteCurrentWeight(@PathVariable(value = "id") Long id, Model model) {
-        this.dailyLogService.deleteById(id);
         log.info("  GetMapping deleteCurrentWeight is called ");
+
+        this.dailyLogService.deleteById(id);
 
         return  REDIRECT + DAILY_LOG;
     }
@@ -227,10 +131,8 @@ public class DailyLogController {
     @GetMapping("/weightOptions")
     public String weightOptions(Model model) {
         log.info("Getmapping weightOptions called in DailylogController");
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        Users loggedInUser = usersService.findByUserName(auth.getName());
 
-        model.addAttribute("user", loggedInUser);
+        model.addAttribute("user", usersService.getLoggedInUser());
         return WEIGHT_OPTIONS;
     }
 
@@ -238,10 +140,9 @@ public class DailyLogController {
     public String saveWeightOptions(@ModelAttribute("user") Users user){
         log.info("Postmappting saveWeightOption called in DailylogController"+user.getKcal_modifier());
 
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        Users loggedInUser = usersService.findByUserName(auth.getName());
-        loggedInUser.setKcal_modifier(user.getKcal_modifier());
-        usersService.save(loggedInUser);
+        usersService.getLoggedInUser().setKcal_modifier(user.getKcal_modifier());
+        usersService.save(usersService.getLoggedInUser());
+
         return REDIRECT + DAILY_LOG;
     }
 
---
+++ src/main/java/com/base/site/models/DailyLogWrapper.java
@@ -0,0 +1,146 @@
+package com.base.site.models;
+
+import com.base.site.services.DailyLogService;
+import com.base.site.services.DailyLogServiceImpl;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DailyLogWrapper {
+    DailyLogService dailyLogService;
+
+    List<DailyLog> dailyLogs = new ArrayList<>();
+    List<DailyLog> dailyLogsFoods = new ArrayList<>();
+    List<DailyLog> dailyLogsBreakfast = new ArrayList<>();
+    List<DailyLog> dailyLogsLunch = new ArrayList<>();
+    List<DailyLog> dailyLogsDinner = new ArrayList<>();
+    List<DailyLog> dailyLogsMiscellaneous = new ArrayList<>();
+    List<DailyLog> dailyLogsPrivateFoods = new ArrayList<>();
+    List<DailyLog> dailyLogsExercises = new ArrayList<>();
+
+    DailyLog weight;
+
+    Food nutrition;
+
+    public DailyLogWrapper(List<DailyLog> dailyLogs) {
+        this.dailyLogs = dailyLogs;
+
+    }
+
+    public DailyLogService getDailyLogService() {
+        return dailyLogService;
+    }
+
+    public void setDailyLogService(DailyLogService dailyLogService) {
+        this.dailyLogService = dailyLogService;
+    }
+
+    public List<DailyLog> getDailyLogs() {
+        return dailyLogs;
+    }
+
+    public void setDailyLogs(List<DailyLog> dailyLogs) {
+        this.dailyLogs = dailyLogs;
+    }
+
+    public List<DailyLog> getDailyLogsFoods() {
+        return dailyLogsFoods;
+    }
+
+    public void setDailyLogsFoods(List<DailyLog> dailyLogsFoods) {
+        this.dailyLogsFoods = dailyLogsFoods;
+    }
+
+    public void addToDailyLogsFoods(DailyLog dailyLog) {
+        dailyLogsFoods.add(dailyLog);
+    }
+
+    public List<DailyLog> getDailyLogsBreakfast() {
+        return dailyLogsBreakfast;
+    }
+
+    public void setDailyLogsBreakfast(List<DailyLog> dailyLogsBreakfast) {
+        this.dailyLogsBreakfast = dailyLogsBreakfast;
+    }
+
+    public void addToDailyLogsBreakfast(DailyLog dailyLog) {
+        dailyLogsBreakfast.add(dailyLog);
+    }
+
+    public List<DailyLog> getDailyLogsLunch() {
+        return dailyLogsLunch;
+    }
+
+    public void setDailyLogsLunch(List<DailyLog> dailyLogsLunch) {
+        this.dailyLogsLunch = dailyLogsLunch;
+    }
+
+    public void addToDailyLogsLunch(DailyLog dailyLog) {
+        dailyLogsLunch.add(dailyLog);
+    }
+
+    public List<DailyLog> getDailyLogsDinner() {
+        return dailyLogsDinner;
+    }
+
+    public void setDailyLogsDinner(List<DailyLog> dailyLogsDinner) {
+        this.dailyLogsDinner = dailyLogsDinner;
+    }
+
+    public void addToDailyLogsDinner(DailyLog dailyLog) {
+        dailyLogsDinner.add(dailyLog);
+    }
+
+    public List<DailyLog> getDailyLogsMiscellaneous() {
+        return dailyLogsMiscellaneous;
+    }
+
+    public void setDailyLogsMiscellaneous(List<DailyLog> dailyLogsMiscellaneous) {
+        this.dailyLogsMiscellaneous = dailyLogsMiscellaneous;
+    }
+
+    public void addToDailyLogsMiscellaneous(DailyLog dailyLog) {
+        dailyLogsMiscellaneous.add(dailyLog);
+    }
+
+    public List<DailyLog> getDailyLogsPrivateFoods() {
+        return dailyLogsPrivateFoods;
+    }
+
+    public void setDailyLogsPrivateFoods(List<DailyLog> dailyLogsPrivateFoods) {
+        this.dailyLogsPrivateFoods = dailyLogsPrivateFoods;
+    }
+
+    public void addToDailyLogsPrivateFoods(DailyLog dailyLog) {
+        dailyLogsPrivateFoods.add(dailyLog);
+    }
+
+    public List<DailyLog> getDailyLogsExercises() {
+        return dailyLogsExercises;
+    }
+
+    public void setDailyLogsExercises(List<DailyLog> dailyLogsExercises) {
+        this.dailyLogsExercises = dailyLogsExercises;
+    }
+
+    public void addToDailyLogsExercises(DailyLog dailyLog) {
+        dailyLogsExercises.add(dailyLog);
+    }
+
+    public DailyLog getWeight() {
+        return weight;
+    }
+
+    public void setWeight(DailyLog weight) {
+        this.weight = weight;
+    }
+
+    public Food getNutrition() {
+        return nutrition;
+    }
+
+    public void setNutrition(Food nutrition) {
+        this.nutrition = nutrition;
+    }
+}
