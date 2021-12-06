13ae0f0518bf19ac2d430d09fd715ff63ea26467
Hasan <36068229+hasandk@users.noreply.github.com>
2021-12-06 23:03:00 +0100

    Add Food to DailyLog is paginated- PrivateFood should be added to

--- src/main/java/com/base/site/services/PrivateFoodServiceImpl.java
+++ src/main/java/com/base/site/services/PrivateFoodServiceImpl.java
@@ -5,10 +5,15 @@ import com.base.site.models.Food;
 import com.base.site.models.PrivateFood;
 import com.base.site.repositories.PrivateFoodRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.data.domain.PageRequest.of;
 
 @Service("PrivateFoodService")
 public class PrivateFoodServiceImpl implements PrivateFoodService{
@@ -24,7 +29,8 @@ public class PrivateFoodServiceImpl implements PrivateFoodService{
     @Override
     public List<PrivateFood> findAllByKeyword(String keyword) {
         if (keyword != null) {
-            return privateFoodRepo.search(keyword);
+            //return privateFoodRepo.search(keyword);
+            return privateFoodRepo.findAll();
         }
         return (List<PrivateFood>) privateFoodRepo.findAll();    }
 
@@ -42,6 +48,20 @@ public class PrivateFoodServiceImpl implements PrivateFoodService{
     @Override
     public void deleteById(Long id) {
         this.privateFoodRepo.deleteById(id);
+    }
+
+
+    @Override
+    public Page<PrivateFood> findPaginatedAddFood(int pageNo, int pageSize, String sortField, String sortDirection, String keyword) {
+        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending():
+                Sort.by(sortField).descending();
+
+        Pageable pageable = of(pageNo - 1, pageSize, sort);
+
+        if (keyword != null) {
+            return privateFoodRepo.findAll(keyword, pageable);
+        }
+        return this.privateFoodRepo.findAll(pageable);
     }
 
 }
